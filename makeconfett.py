35fa945919fec30e57b4f2c514f3435807a717ef
epcnes <75555542+epcnes@users.noreply.github.com>
2021-11-18 21:25:52 -0500

    just a little broken

--- makeconfett.py
+++ makeconfett.py
@@ -5,36 +5,30 @@ import os.path
 import tkinter as tk
 from win32api import GetSystemMetrics
 
-numofConfett = randint(10,20)
+numofConfett = randint(2,5)
 Confett = []
+gifs = []
 winwidth = GetSystemMetrics(0)
 winheight = GetSystemMetrics(1)
 
-wn = tk.Canvas(width=winwidth, height=winheight, highlightthickness=0, bd=0)
+root = tk.Tk()
+wn = tk.Canvas(master = root, width=winwidth, height=winheight, highlightthickness=0, bd=0)
+wn.pack()
 
-def maker():
-    gifs = []
-    win = []
-    #getting gifs#
-    path = os.getcwd() + "\\Coloured Confett"
-    directlist = os.listdir(path)
-    for i in range(0, len(directlist)-1):
-        gifs.append(f"{path}//{directlist[i]}")
-    for i in range(0, numofConfett-1):
-        globals()[f"confetti#{i}"] = tk.PhotoImage(file=gifs[randint(0, len(gifs)-1)])
-        win.append(globals()[f"confetti#{i}"])
-    window = tk.Tk()
-    window.config(highlightbackground='black')
-    window.overrideredirect(True)
-    window.wm_attributes('-transparent', 'black')
+#getting gifs#
+path = os.getcwd() + "\\Coloured Confett"
+directlist = os.listdir(path)
+for i in range(0, len(directlist)-1):
+    gifs.append(f"{path}//{directlist[i]}")
 
-maker()  
 
-# for i in range(0, numofConfett-1):
-#     globals()[f"turts{i}"] = turtle.Turtle()
-#     Confett.append(globals()[f"turts{i}"])
-#     Confett[i].hideturtle()
-#     Confett[i].speed(speed=0)
-#     globals()[f"turts{i}"].shape(gifs[randint(0, len(gifs)-1)])
-#     Confett[i].penup()
-#     Confett[i].right(90)
+for i in range(0, numofConfett-1):
+    globals()[f"turts{i}"] = turtle.Turtle(wn)
+    Confett.append(globals()[f"turts{i}"])
+    Confett[i].hideturtle()
+    Confett[i].speed(speed=0)
+    globals()[f"turts{i}"].shape(gifs[randint(0, len(gifs)-1)])
+    Confett[i].penup()
+    Confett[i].right(90)
+
+root.mainloop()
