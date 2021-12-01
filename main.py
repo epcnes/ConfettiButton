0032aabb869ad803431fac79d119b1b5575776bc
epcnes <75555542+epcnes@users.noreply.github.com>
2021-11-30 21:49:57 -0500

    yes

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
 from random import randint
 from turtle import forward, left, right, penup
 import time
-from makeconfett import Confett, winheight, winwidth
+import turtle
+import os
+import tkinter as tk
+from win32api import GetSystemMetrics
 
+#defining things#
+gifs = []
+winwidth = GetSystemMetrics(0)
+winheight = GetSystemMetrics(1)
 width = winwidth/2
+
+#make window#
+root = tk.Tk()
+root.title("Confetti")
+root.config(highlightbackground='black')
+root.wm_attributes('-transparentcolor', 'black')
+root.overrideredirect(True)
+
+canvas = tk.Canvas(master=root, width=winwidth, height=winheight, highlightthickness=0)
+canvas.pack()
+
+wn = turtle.TurtleScreen(canvas)
+wn.screensize(winwidth, winheight)
+Confett = turtle.RawTurtle(wn)
+Confett.hideturtle()
+Confett.penup()
+
+#getting gifs#
+path = os.getcwd() + "\\Coloured Confett"
+directlist = os.listdir(path)
+for i in range(0, len(directlist)):
+    gifs.append(f"{path}\\{directlist[i]}")
+    wn.addshape(gifs[i])
+#random confetti#
+Confett.shape(gifs[randint(0, len(gifs)-1)])
 
 def movement(temp1):
     temp1.right(90)
@@ -14,12 +46,12 @@ def movement(temp1):
         if (side == 3):
             #go right#
             temp1.left(90)
-            temp1.forward(randint(1,5))
+            temp1.forward(randint(1,10))
             temp1.right(90)
             temp1.forward(randint(10, 50))
         elif (side == 5):
             temp1.right(90)
-            temp1.forward(randint(1,5))
+            temp1.forward(randint(1,10))
             temp1.left(90)
             temp1.forward(randint(10, 50))
         else:
